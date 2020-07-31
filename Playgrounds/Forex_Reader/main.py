8abaf892cf48fdbe0666bc94047c7ab16f932e16
AdrKacz <adrien.kaczmarek@gmail.com>
2020-07-31 20:01:17 +0200

    Forex Reader Improvement
    Look at the source of Yahoo Finance to find a way to get the numbers easily. Next step is to create a library with various frequency to train an RL algorithm

--- Playgrounds/Forex_Reader/main.py
+++ Playgrounds/Forex_Reader/main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from time import time
 
 """
 Program that look for currency value (FOREX)
@@ -15,6 +16,12 @@ class Website:
 
 
 class Crawler:
+	"""
+	Crawler that its made for Yahoo Finance
+	"""
+	def __init__(self, website):
+		self.website = website
+
 	def get_page(self, url):
 		try:
 			request = requests.get(url)
@@ -22,13 +29,18 @@ class Crawler:
 			return None
 		return BeautifulSoup(request.text, 'html.parser')
 
-	def search(self, website):
-		soup = self.get_page(website.url)
-		return soup
+	def get_value(self):
+		soup = self.get_page(self.website.url)
+		quote = soup.select("#quote-header-info")[0]
+		section = quote.select("div")
+		return float(section[-2].select("span")[0].get_text().replace(",", "."))
 
 
 website = Website("Yahoo Finance", "https://fr.finance.yahoo.com/quote/EURUSD=X?p=EURUSD=X&.tsrc=fin-srch")
 
-crawler = Crawler()
+crawler = Crawler(website)
 
-soup = crawler.search(website)
+def measure():
+	t1 = time()
+	print(crawler.get_value())
+	return time() - t1
