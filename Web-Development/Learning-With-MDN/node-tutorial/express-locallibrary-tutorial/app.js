9b25ba81503fbb4c3e3bf8f03a2dffe4560be362
AdrKacz <61620013+AdrKacz@users.noreply.github.com>
2020-09-15 21:48:20 +0200

    Commit of the Day
    Following MDN tutorial about Node.js. Used Pug and remade route (not 
    save last time, kept on my old computer, thus had to rewrite it quickly)

--- Web-Development/Learning-With-MDN/node-tutorial/express-locallibrary-tutorial/app.js
+++ Web-Development/Learning-With-MDN/node-tutorial/express-locallibrary-tutorial/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
 
 var app = express();
 
@@ -14,7 +15,7 @@ var mongoose = require('mongoose');
 var mongoDB = 'mongodb+srv://AdrKaczUser:SYVbwZ4p8nzTbuEk@cluster0.pjdxs.azure.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
-db.on('error'n console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
